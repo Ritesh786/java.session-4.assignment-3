6e44f0c779be211ffb1ab846dfbb3257715981e2
Ritesh786 <jharitesh6@gmail.com>
2016-07-21 12:10:18 +0530

    session4 assignment 3
    session4 assignment 3

---
+++ Emp.java
@@ -0,0 +1,27 @@
+import javax.swing.Spring;
+
+public class Emp {
+private int salary;
+private String name;
+private int age;
+private int getSalary() {
+	return salary;
+}
+public void setSalary(int salary) {
+	this.salary = salary;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+
+
+}
