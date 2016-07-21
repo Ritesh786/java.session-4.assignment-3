cd54442371706a7a3358d3828176cecd18abca5e
Ritesh786 <jharitesh6@gmail.com>
2016-07-21 12:16:41 +0530

    new assignment 3
    new assignment 3

---
+++ Emp2.java
@@ -0,0 +1,15 @@
+
+public class Emp2 {
+
+	public static void main(String[] args) {
+		Emp ob = new Emp();
+		ob.setSalary(20000);
+		ob.setName("Goslin");
+		ob.setAge(50);
+		System.out.println("Employee Name is "+ob.getName());
+		System.out.println("Employee age is "+ob.getAge());
+		System.out.println("Employee Salary is "+ob.getSalary());
+
+	}
+
+}
